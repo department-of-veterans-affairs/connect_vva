935fdf152fbc09f571d2b619bf60b12c4b9156c6
aroltsch <anya.roltsch@gmail.com>
2017-10-12 15:35:57 -0400

    Add a comment

--- lib/httpi/net_http/cipher.rb
+++ lib/httpi/net_http/cipher.rb
@@ -4,6 +4,10 @@ module HTTPI
 
       private
 
+      # Savon uses the HTTPi library for the network requests but this library
+      # does not support setting SSL ciphers
+      # The following patch redefines net_http setup_ssl_auth to explicitly
+      # deny the DH ciphers in the list of allowed SSL ciphers
       def setup_ssl_auth
         super
         @client.ciphers = ["DEFAULT:!DH"]
