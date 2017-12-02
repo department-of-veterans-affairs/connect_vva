39351ed6c5b8318dfa390af7d6fb97cf0e257b93
Artem Vovk <Artem.Vovk@va.gov>
2017-12-02 17:06:18 -0500

    naive merge

--- lib/vva/base.rb
+++ lib/vva/base.rb
@@ -95,7 +95,8 @@ module VVA
 
     # Proxy to call a method on our web service.
     def request(method, message)
-      client.wsdl.request.headers = { "Host" => domain } if @forward_proxy_url
+      client.wsdl.request.headers = { "service" => method.name.to_s }
+      client.wsdl.request.headers.merge({ "Host" => domain }) if @forward_proxy_url
       client.call(method, message: message)
     rescue Savon::SOAPFault => e
       raise VVA::SOAPError.new(e)
